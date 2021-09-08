88e68faf2d748fb3cce66664067166ff7836d014
Nicholas Duffy <3457341+duffn@users.noreply.github.com>
2021-09-07 19:31:01 -0600

    Add deployments list endpoint

---
+++ alertlogic/deployments.go
@@ -0,0 +1,92 @@
+package alertlogic
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+type Deployment struct {
+	Version       int64           `json:"version,omitempty"`
+	Status        Status          `json:"status,omitempty"`
+	Scope         Scope           `json:"scope,omitempty"`
+	Scan          bool            `json:"scan,omitempty"`
+	Platform      Platform        `json:"platform,omitempty"`
+	Name          string          `json:"name,omitempty"`
+	Modified      ModifiedCreated `json:"modified,omitempty"`
+	Mode          string          `json:"mode,omitempty"`
+	ID            string          `json:"id,omitempty"`
+	Enabled       bool            `json:"enabled,omitempty"`
+	Discover      bool            `json:"discover,omitempty"`
+	Credentials   []Credential    `json:"credentials,omitempty"`
+	Created       ModifiedCreated `json:"created,omitempty"`
+	CloudDefender CloudDefender   `json:"cloud_defender,omitempty"`
+	AccountID     string          `json:"account_id,omitempty"`
+}
+
+type CloudDefender struct {
+	Enabled    bool   `json:"enabled,omitempty"`
+	LocationID string `json:"location_id,omitempty"`
+}
+
+type Credential struct {
+	ID      string `json:"id,omitempty"`
+	Purpose string `json:"purpose,omitempty"`
+	Version string `json:"version,omitempty"`
+}
+
+type Platform struct {
+	Type    string  `json:"type,omitempty"`
+	ID      string  `json:"id,omitempty"`
+	Monitor Monitor `json:"monitor,omitempty"`
+	Default bool    `json:"default,omitempty"`
+}
+
+type Monitor struct {
+	Enabled         bool   `json:"enabled,omitempty"`
+	CTInstallRegion string `json:"ct_install_region,omitempty"`
+}
+
+type Scope struct {
+	Include []Include `json:"include,omitempty"`
+	Exclude []Exclude `json:"exclude,omitempty"`
+}
+
+type Exclude struct {
+	Type string `json:"type,omitempty"`
+	Key  string `json:"key,omitempty"`
+}
+
+type Include struct {
+	Type   string `json:"type,omitempty"`
+	Key    string `json:"key,omitempty"`
+	Policy Policy `json:"policy,omitempty"`
+}
+
+type Policy struct {
+	ID string `json:"id,omitempty"`
+}
+
+type Status struct {
+	Status  string `json:"status,omitempty"`
+	Updated int64  `json:"updated,omitempty"`
+}
+
+// ListDeployments lists deployments for an account.
+//
+// API reference: https://console.cloudinsight.alertlogic.com/api/deployments/#api-Resources-ListDeployments
+func (api *API) ListDeployments() ([]Deployment, error) {
+	res, _, err := api.makeRequest("GET", fmt.Sprintf("%s/%s/deployments", deploymentServicePath, api.AccountID), nil, nil, nil)
+	if err != nil {
+		return []Deployment{}, errors.Wrap(err, errMakeRequestError)
+	}
+
+	var r []Deployment
+	err = json.Unmarshal(res, &r)
+	if err != nil {
+		return []Deployment{}, errors.Wrap(err, errUnmarshalError)
+	}
+
+	return r, nil
+}
---
+++ alertlogic/deployments_test.go
@@ -0,0 +1,341 @@
+package alertlogic
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	listDeploymentsPath = fmt.Sprintf("/%s/%s/deployments", deploymentServicePath, testAccountId)
+)
+
+func TestDeployments_ListDeployments(t *testing.T) {
+	setup()
+	defer teardown()
+
+	const response = `[
+	{
+		"id": "AF50CF2D-3E77-46E3-B003-0262D77B2A65",
+		"account_id": "01000001",
+		"name": "AWS Production Deployment",
+		"platform": {
+			"type": "aws",
+			"id": "111111111111",
+			"monitor": {
+				"enabled": true,
+				"ct_install_region": "us-east-1"
+			}
+		},
+		"mode": "automatic",
+		"enabled": true,
+		"discover": true,
+		"scan": true,
+		"scope": {
+			"include": [
+				{
+					"type": "region",
+					"key": "/aws/us-east-1"
+				},
+				{
+					"type": "region",
+					"key": "/aws/us-east-2",
+					"policy": {
+						"id": "A8E8B104-8F45-411D-A240-A30EA5FE25B0"
+					}
+				},
+				{
+					"type": "vpc",
+					"key": "/aws/us-west-1/vpc/vpc-12345678",
+					"policy": {
+						"id": "D12D5E67-166C-474F-87AA-6F86FC9FB9BC"
+					}	
+				}
+			],
+			"exclude": [
+				{
+					"type": "region",
+					"key": "/aws/ap-southeast-1"
+				}
+			]
+		},
+		"cloud_defender": {
+			"enabled": true,
+			"location_id": "defender-us-denver"
+		},
+		"credentials": [
+			{
+				"id": "E09F0AF8-18F8-49CE-B9AC-01C3E214B4EB",
+				"purpose": "discover",
+				"version": "2018-01-01"
+			},
+			{
+				"id": "D9B5066E-6145-4D41-9B3A-C123BDE64ABD",
+				"purpose": "x-account-monitor",
+				"version": "2018-01-01"
+			}
+		],
+		"status": {
+			"status": "ok",
+			"updated": 1493671342
+		},
+		"created": {
+			"at": 1496673172,
+			"by": "BBCAE827-22A6-433D-884E-22AABF2DC82B"
+		},
+		"modified": {
+			"at": 1496673245,
+			"by": "1443C74F-2AF7-4D7F-BF4A-FDFFE9C67182"
+		},
+		"version": 2
+	},
+	{
+		"id": "86975343-2DA7-4E77-9F52-8488C4217191",
+		"account_id": "01000001",
+		"name": "Azure Production Deployment",
+		"platform": {
+			"type": "azure",
+			"id": "93B0107D-21AD-48B0-9101-C60F20E09966"
+		},
+		"mode": "manual",
+		"enabled": true,
+		"discover": true,
+		"scan": true,
+		"scope": {
+			"include": [
+				{
+					"type": "location",
+					"key": "/subscriptions/93B0107D-21AD-48B0-9101-C60F20E09966/locations/centralus"
+				},
+				{
+					"type": "location",
+					"key": "/subscriptions/93B0107D-21AD-48B0-9101-C60F20E09966/locations/canadacentral",
+					"policy": {
+						"id": "A8E8B104-8F45-411D-A240-A30EA5FE25B0"
+					}
+				},
+				{
+					"type": "vnet",
+					"key": "/subscriptions/93B0107D-21AD-48B0-9101-C60F20E09966/resourceGroups/test-rg-01/providers/Microsoft.Network/virtualNetworks/test-vnet-01",
+					"policy": {
+						"id": "D12D5E67-166C-474F-87AA-6F86FC9FB9BC"
+					}
+				}
+			],
+			"exclude": [
+				{
+					"type": "location",
+					"key": "/subscriptions/93B0107D-21AD-48B0-9101-C60F20E09966/locations/koreasouth"
+				}
+			]
+		},
+		"cloud_defender": {
+			"enabled": true,
+			"location_id": "defender-us-denver"
+		},
+		"credentials": [
+			{
+				"id": "E09F0AF8-18F8-49CE-B9AC-01C3E214B4EB",
+				"purpose": "discover",
+				"version": "2018-01-01"
+			}
+		],
+		"status": {
+			"status": "ok",
+			"updated": 1493671342
+		},
+		"created": {
+			"at": 1496673172,
+			"by": "BBCAE827-22A6-433D-884E-22AABF2DC82B"
+		},
+		"modified": {
+			"at": 1496673245,
+			"by": "1443C74F-2AF7-4D7F-BF4A-FDFFE9C67182"
+		},
+		"version": 4
+	}]`
+
+	mux.HandleFunc(listDeploymentsPath, func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "GET", r.Method, "Expected method 'GET', got %s", r.Method)
+
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, response)
+	})
+
+	want := []Deployment{
+		{
+			Version: 2,
+			Status: Status{
+				Status:  "ok",
+				Updated: 1493671342,
+			},
+			Scope: Scope{
+				Include: []Include{
+					{
+						Type:   "region",
+						Key:    "/aws/us-east-1",
+						Policy: Policy{ID: ""},
+					},
+					{
+						Type:   "region",
+						Key:    "/aws/us-east-2",
+						Policy: Policy{ID: "A8E8B104-8F45-411D-A240-A30EA5FE25B0"},
+					},
+					{
+						Type:   "vpc",
+						Key:    "/aws/us-west-1/vpc/vpc-12345678",
+						Policy: Policy{ID: "D12D5E67-166C-474F-87AA-6F86FC9FB9BC"},
+					},
+				},
+				Exclude: []Exclude{
+					{
+						Type: "region",
+						Key:  "/aws/ap-southeast-1",
+					},
+				},
+			},
+			Scan: true,
+			Platform: Platform{
+				Type: "aws",
+				ID:   "111111111111",
+				Monitor: Monitor{
+					Enabled:         true,
+					CTInstallRegion: "us-east-1",
+				},
+				Default: false,
+			},
+			Name: "AWS Production Deployment",
+			Modified: ModifiedCreated{
+				At: 1496673245,
+				By: "1443C74F-2AF7-4D7F-BF4A-FDFFE9C67182",
+			},
+			Mode:     "automatic",
+			ID:       "AF50CF2D-3E77-46E3-B003-0262D77B2A65",
+			Enabled:  true,
+			Discover: true,
+			Credentials: []Credential{
+				{
+					ID:      "E09F0AF8-18F8-49CE-B9AC-01C3E214B4EB",
+					Purpose: "discover",
+					Version: "2018-01-01",
+				},
+				{
+					ID:      "D9B5066E-6145-4D41-9B3A-C123BDE64ABD",
+					Purpose: "x-account-monitor",
+					Version: "2018-01-01",
+				},
+			},
+			Created: ModifiedCreated{
+				At: 1496673172,
+				By: "BBCAE827-22A6-433D-884E-22AABF2DC82B",
+			},
+			CloudDefender: CloudDefender{
+				Enabled:    true,
+				LocationID: "defender-us-denver",
+			},
+			AccountID: "01000001",
+		},
+		{
+			Version: 4,
+			Status: Status{
+				Status:  "ok",
+				Updated: 1493671342,
+			},
+			Scope: Scope{
+				Include: []Include{
+					{
+						Type:   "location",
+						Key:    "/subscriptions/93B0107D-21AD-48B0-9101-C60F20E09966/locations/centralus",
+						Policy: Policy{ID: ""},
+					},
+					{
+						Type:   "location",
+						Key:    "/subscriptions/93B0107D-21AD-48B0-9101-C60F20E09966/locations/canadacentral",
+						Policy: Policy{ID: "A8E8B104-8F45-411D-A240-A30EA5FE25B0"},
+					},
+					{
+						Type:   "vnet",
+						Key:    "/subscriptions/93B0107D-21AD-48B0-9101-C60F20E09966/resourceGroups/test-rg-01/providers/Microsoft.Network/virtualNetworks/test-vnet-01",
+						Policy: Policy{ID: "D12D5E67-166C-474F-87AA-6F86FC9FB9BC"},
+					},
+				},
+				Exclude: []Exclude{
+					{
+						Type: "location",
+						Key:  "/subscriptions/93B0107D-21AD-48B0-9101-C60F20E09966/locations/koreasouth",
+					},
+				},
+			},
+			Scan: true,
+			Platform: Platform{
+				Type: "azure",
+				ID:   "93B0107D-21AD-48B0-9101-C60F20E09966",
+				Monitor: Monitor{Enabled: false,
+					CTInstallRegion: "",
+				},
+				Default: false,
+			},
+			Name: "Azure Production Deployment",
+			Modified: ModifiedCreated{
+				At: 1496673245,
+				By: "1443C74F-2AF7-4D7F-BF4A-FDFFE9C67182",
+			},
+			Mode:     "manual",
+			ID:       "86975343-2DA7-4E77-9F52-8488C4217191",
+			Enabled:  true,
+			Discover: true,
+			Credentials: []Credential{
+				{
+					ID:      "E09F0AF8-18F8-49CE-B9AC-01C3E214B4EB",
+					Purpose: "discover",
+					Version: "2018-01-01",
+				},
+			},
+			Created: ModifiedCreated{
+				At: 1496673172,
+				By: "BBCAE827-22A6-433D-884E-22AABF2DC82B",
+			},
+			CloudDefender: CloudDefender{
+				Enabled:    true,
+				LocationID: "defender-us-denver",
+			},
+			AccountID: "01000001",
+		},
+	}
+
+	deployments, err := client.ListDeployments()
+	if assert.NoError(t, err) {
+		assert.Equal(t, deployments, want)
+	}
+}
+
+func TestDeployments_ListDeploymentsMakeRequestError(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc(listDeploymentsPath, func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "GET", r.Method, "Expected method 'GET', got %s", r.Method)
+		w.WriteHeader(http.StatusUnauthorized)
+	})
+
+	_, err := client.ListDeployments()
+	assert.Error(t, err)
+	assert.Equal(t, err.Error(), "error from makeRequest: HTTP status 401: invalid credentials")
+}
+
+func TestDeployments_ListDeploymentsUnmarshalError(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc(listDeploymentsPath, func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "GET", r.Method, "Expected method 'GET', got %s", r.Method)
+		fmt.Fprintf(w, "not json")
+	})
+
+	_, err := client.ListDeployments()
+
+	assert.Error(t, err)
+	assert.Equal(t, err.Error(), testUnmarshalError)
+}
